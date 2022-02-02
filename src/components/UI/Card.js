e72fea6912f3752a94d7d5694fc5cffede8bf9c2
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-02-02 16:46:33 +0300

    Adding css module styles/classes to Card component, wrapping component IMPORTANT

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import classes from './Card.module.css';
+
 const Card = (props) => {
   // Take note of the special children prop!. this is to make components that wrap other components "children"
-  return <div>{props.children}</div>;
+  return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
+    // Take note of props.className! this came from the AddUser.js in the written <Card> component.
 };
 
 export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../UI/Card";
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -9,7 +10,9 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card>
+    // Take Note of the className attribute, since this is a custom component, it can't use the real className attribut we normally use with built-in html elements. In this case it will work as props
+    // So the next step is to go back to the Card.js component and make sure to accept the incoming className prop
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         {/* For accessibility add the "for" and "id" attributes to the lable and input respectfully, helps people with screen readers. but take note of how we write the for attribute, because for is a reserved js word and we're using JSX. This is how we connect the label to the input */}
         <label htmlFor="username">Username</label>
