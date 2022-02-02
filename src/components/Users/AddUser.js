e727f3b8ac1acd3808679fe353e54aee59f17f25
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-02-02 15:27:08 +0300

    initial commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AddUser = (props) => {
+
+    const addUserHandler = event => {
+        // preventdefault to prevent the behavior for this type of event, in this case that a request would be sent.
+        event.preventDefault();
+    };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      {/* For accessibility add the "for" and "id" attributes to the lable and input respectfully, helps people with screen readers. but take note of how we write the for attribute, because for is a reserved js word and we're using JSX. This is how we connect the label to the input */}
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text"></input>
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number"></input>
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
