53e0ca9cfd165870b6c2899eda91af8abd169388
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-02-07 11:28:09 +0300

    React.Fragment instead of wrapper component

--- src/components/Helpers/Wrapper.js
+++ src/components/Helpers/Wrapper.js
@@ -7,3 +7,7 @@ const Wrapper = props => {
 };
 
 export default Wrapper;
+
+// Later in the course we were introduced to built in react wrappers called "fragments"
+// in some build configuration like this one we could also use empty tags <> JSX </>, but doesnt always work.
+// We are using React.Fragment in App.js
